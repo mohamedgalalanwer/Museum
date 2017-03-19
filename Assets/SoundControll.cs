9d82fec4f8055942fca2792f9956870b6ce5c5d8
mohamedgalalanwer <abogalalmedo@gmail.com>
2017-03-19 07:00:40 +0200

    update
    This specification requires that each exhibit have its own unique audio
    clip, relevant to the content there.
    This audio could play on the player's approach or by clicking on a
    designated button maybe.

---
+++ Assets/SoundControll.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundControll : MonoBehaviour {
+    AudioSource play;
+    GvrAudioSource stop;
+
+	// Use this for initialization
+	void Start () {
+       play = GetComponent<AudioSource>();
+        //stop = GetComponent<GvrAudioSource>();
+	}
+	
+	// Update is called once per frame
+	public void PlayAudio () {
+        Debug.Log("method called");
+        play.Play();
+	}
+}
--- README.txt
+++ README.txt
@@ -7,5 +7,12 @@ challenging in complete the task in a short time
 
 
 
+Welcome to the Egyptian Museum will offer you a collection of historical images of Pharaohs so far when 
+approaching the image will hear this image and personality which shows you information about the profile,
+ we wish you happy holidays in the Egyptian Museum
+
+
+
+
 GVR Unity SDK v1.0
  
